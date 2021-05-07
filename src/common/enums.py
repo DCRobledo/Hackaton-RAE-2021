988cfad62bf81fd50506c2f74d75a3c2cfb9a021
Daniel <daniel.canorobledo@gmail.com>
2021-05-08 00:11:52 +0200

    implemented both metaclass Singleton and enums

---
+++ src/common/enums.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+class text_scope(Enum):
+    ENERGY = 1
+    ENVIRONMENT = 2
+    OTHER = 3
+    
+class word_type(Enum):
+    NOUN = 1
+    PRONOUN = 2
+    VERB = 3
+    ADJECTIVE = 4
+    ADVERB = 5
+    PREPOSITION = 6
+    CONJUNCTION = 7
+    INTERJECTION = 8
+    
+class word_category(Enum):
+    NEOLOGISM = 1
+    TECHNICISM = 2
+    FOREIGN_WORD = 3
+    OTHER = 4
+    
+class word_scope(Enum):
+    ENERGY = 1
+    ENVIRONMENT = 2
+    OTHER = 3
+    
+class word_composition(Enum):
+    DERIVATIVE = 1
+    COMPOUND = 2
+    OTHER = 3
+    
+class filter_type(Enum):
+    CATEGORY = 1
+    SCOPE = 2
+    COMPOSITION = 3
+    
+class analyzer_goal(Enum):
+    FRECUENCY = 1
+    TECHNICISMS = 2
+    FOREIGN_WORDS = 3
+    MOST_FRECUENT_TYPE = 4
+    DEFINITION = 5
+    
+class tuple_type(Enum):
+    FRECUENCY = 1
+    TECHNICISMS = 2
+    FOREIGN_WORDS = 3
+    MOST_FRECUENT_TYPE = 4
+    DEFINITION = 5
+    
+class graphic_type(Enum):
+    TABLE = 1
+    GRAPH = 2
+    
+class graph_type(Enum):
+    BAR = 1
+    LINEAR = 2
+    
